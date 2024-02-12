ceb19d0e34e3e3a7ce605e304b8a5c281d29ffde
manojnagaraj01 <n.manoj9535@gmail.com>
2024-02-12 13:22:42 +0530

    project

--- src/components/pages/Projects/Data/Data.js
+++ src/components/pages/Projects/Data/Data.js
@@ -2,32 +2,32 @@ let projectData=[
     {
         name:"HTML Blog Page",
         img:"/project-images/html-img.png",
-        link:""
+        link:"https://manojnagaraj01.github.io/Project-HTML/"
     },
     {
         name:"Todo App UI Web",
         img:"/project-images/html-css.png",
-        link:""
+        link:"https://manojnagaraj01.github.io/TODO-WEB/"
     },
     {
         name:"Reminder Clock App",
         img:"/project-images/javascript.png",
-        link:""
+        link:"https://manojnagaraj01.github.io/PROJECT-Remainder-Clock/"
     },
     {
         name:"Todo App JS",
         img:"/project-images/todo-js.png",
-        link:""
+        link:"https://manojnagaraj01.github.io/TODO_js/"
     },
     {
         name:"React Blog Web App",
         img:"/project-images/react-blog.png",
-        link:""
+        link:"https://react-blog-app-psi.vercel.app/"
     },
     {
         name:"Ecommerce Web App",
         img:"/project-images/e-commerce-react.png",
-        link:""
+        link:"https://e-commerce-ten-blue-90.vercel.app/"
     },
 ]
 
